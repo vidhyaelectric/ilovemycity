310dc9e60ed0c2d55304dd658837198fb2192e89
vidhyaelectric <vidhyaelectric@gmail.com>
2018-05-15 16:59:02 -0400

    fixes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/',routes.home);
 app.get('/:city',routes.city);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 var server=app.listen(port,function(req,res){
     console.log("Catch the action at http://localhost:"+port);
